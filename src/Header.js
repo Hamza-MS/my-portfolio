57667bc27356bbd74d6f7bb1dd84d7488763df78
Hamza El mesaouab <77578145+Hamza-ton@users.noreply.github.com>
2023-02-23 20:01:08 +0100

    setting up taillwind

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Hamza</h1>
+      <h1 className="text-2xl font-bold text-center pt-10">Hamza</h1>
       <p>A brief introduction about yourself </p>
     </header>
   );
