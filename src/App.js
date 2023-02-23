16d69381513680813b6de658dd12fb94d0f7c565
Hamza El mesaouab <77578145+Hamza-ton@users.noreply.github.com>
2023-02-23 21:42:38 +0100

    adding a navBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavBar from './components/navBar';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <NavBar />
     </div>
   );
 };
