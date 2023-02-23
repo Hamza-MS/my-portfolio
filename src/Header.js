7175090deeeaf755349ca75c55387f182ce1da31
Hamza El mesaouab <77578145+Hamza-ton@users.noreply.github.com>
2023-02-23 19:40:46 +0100

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Hamza</h1>
+      <p>A brief introduction about yourself ...</p>
+    </header>
+  );
+};
+
+export default Header;
